bd2bb2276a6959d31c672cc2d094f0971b0c306a
tangtao <tangtao2099@outlook.com>
2020-04-14 14:46:33 +0800

    tangtao add nav link home at 2020-4-14 14:46:32

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,23 @@
 module.exports = {
+    // web 标题
     title: "TangTao ",
+    // web
     description: "2020-4-14 14:03:08",
+    themeConfig: {
+        logo: "/assets/img/tao-design-logo.d3d934e2.png",
+        nav: [{
+                text: "External",
+                link: "https://google.com",
+                target: "_self",
+                rel: "",
+            },
+            {
+                text: "TangTao",
+                link: "/tangtao/2020",
+                target: "_blank",
+                rel: "",
+            },
+            { text: "Guide", link: "/guide/", target: "_blank" },
+        ],
+    },
 };
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,15 @@
 # readme 2020-4-14 14:02:26
+
+
+
+
+
+![tangtao](./.vuepress/public/tao-design-logo.png)
+
+
+
+
+
+
+
+777777
